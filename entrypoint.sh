1c07a67ee69bb31649cceeb4690d6691c7497e13
somedspcov <129502902+somedspcov@users.noreply.github.com>
2023-04-20 11:10:11 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ WSPATH=${WSPATH:-'argo'}
 UUID=${UUID:-'99606292-25d0-4bcd-ace1-ae57ae2cbeea'}
 
 # 其他Paas保活
-PAAS1_URL=
+PAAS1_URL=https://jimes-somedspcov.b4a.run/
 PAAS2_URL=
 PAAS3_URL=
 PAAS4_URL=
